341ebd46a82de7aa85ac6baf018f26c7eb0cb743
Camilo Arias <j.camilo.ariasospina@gmail.com>
2026-03-27 15:00:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from logica import calcular_promedios
 from google import genai
 
 # 👉 API KEY directa
-client = genai.Client(api_key="AIzaSyDCIEHETLU6wlf2-FydkwIRqOBrLLjkXo8")
+client = genai.Client(api_key="AIzaSyAbIHAtNbGlRiZniYb8ttmrq9PfuyUkTQQ")
 
 # Título
 st.title("Chatbot de promedios (Gemini)")
